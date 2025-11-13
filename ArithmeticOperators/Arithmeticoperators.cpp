c6a6da2169093473f0c3971ddc2c47c5cb68c972
Mahmood Shafi <mahmoodshafiwatto786@gmail.com>
2025-11-13 18:23:20 +0500

    arithematic operators in cpp

---
+++ ArithmeticOperators/Arithmeticoperators.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int savings = 1000; 
+  // savings += 501;
+
+  cout << "Total savings: " << savings;
+  cout << "Modules " << savings % 7;
+  return 0;
+}
---
+++ ArithmeticOperators/moreoperators.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int result1 = 10 - 2 + 5;  
+  int result2 = 10 - (2 + 5);  
+
+  cout << "result of result1: " << result1 << "\n";
+  cout << "result of result2: " << result2 << "\n";
+
+  return 0;
+}
